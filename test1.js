a5c07d3a8a92a65c3a5f1a8c3b11fbf15012ff90
Maksym Ostapchuk <gadget.add@gmail.com>
2023-09-20 12:53:08 +0300

    03-2

--- test1.js
+++ test1.js
@@ -1,2 +1,12 @@
 "use strict";
-let info;
+let info = {
+    officeId: 45,
+    isOpened: false,
+    contacts: {
+        phone: '+38000000000',
+        email: 'my@mail.com',
+        address: {
+            city: 'Kyiv',
+        },
+    },
+};
--- test1.ts
+++ test1.ts
@@ -8,4 +8,14 @@ let info: {
       city: string;
     };
   };
+} = {
+  officeId: 45,
+  isOpened: false,
+  contacts: {
+    phone: '+38000000000',
+    email: 'my@mail.com',
+    address: {
+      city: 'Kyiv',
+    },
+  },
 };
